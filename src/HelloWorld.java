48af6a6b92d49e40f889a1e55f38f006b194995f
Samson Weldegiorgis <S.WELDEGIORGIS7347@edmail.edcc.edu>
2022-03-26 13:48:30 -0700

    Hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Man of the year ");
+        System.out.println("Meeting World");
+    }
+}
+
