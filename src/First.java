2b6af97b57da9758608097fad66b7c5f41238c6b
Samson Weldegiorgis <S.WELDEGIORGIS7347@edmail.edcc.edu>
2022-03-26 13:49:46 -0700

    First

---
+++ src/First.java
@@ -0,0 +1,15 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Working......");
+    }
+
+    // extra line 1
+    // extra line 2
+    // extra line 3
+
+
+    // also don't forget to be there ontime
+
+}
